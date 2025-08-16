ce78877b911715f448f39bf563e68767c90433cd
devjrios <july.rios@protonmail.com>
2025-08-16 17:04:53 -0500

    vscode-java-tests downgrade

--- flake.nix
+++ flake.nix
@@ -3,8 +3,8 @@
 
   inputs = {
     nixvim.url = "github:nix-community/nixvim/nixos-25.05";
-    nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions/028a172309c9eeb8d8f6d9d19130d703d51fde93";
-    nixpkgs-with-working-jdtls.url = "github:NixOS/nixpkgs/62659a8ca7bba5d151365ff641b74439181725c0";
+    nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions/9a1e130619ec0b2e16624aef973f6437029e2d17";
+    nixpkgs-with-working-jdtls.url = "github:NixOS/nixpkgs/ebbd613587758567d62555cfe91a770148e1a30f";
     flake-parts.follows = "nixvim/flake-parts";
     nixpkgs.follows = "nixvim/nixpkgs";
     systems.follows = "nixvim/systems";
